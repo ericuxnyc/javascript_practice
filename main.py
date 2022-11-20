b8f5e1b7ea66536864ca370e7bac6812829de92e
ericuxnyc <86964628+ericuxnyc@users.noreply.github.com>
2022-11-20 09:51:56 -0500

    fetch examples

---
+++ main.py
@@ -0,0 +1,63 @@
+remove last letter of string python
+def remove_last_letter(string):
+    return string[:-1]
+
+# Path: main.py
+def get_last_letter(string):
+    return string[-1]
+
+# Path: main.py
+def get_first_letter(string):
+    return string[0]
+
+# Path: main.py
+def get_first_two_letters(string):
+    return string[:2]
+
+# Path: main.py
+def get_last_two_letters(string):
+    return string[-2:]
+
+# Path: main.py
+def get_middle_letter(string):
+    return string[len(string) // 2]
+
+# Path: main.py
+def get_middle_two_letters(string):
+    return string[(len(string) // 2) - 1:(len(string) // 2) + 1]
+
+# Path: main.py
+def get_middle_three_letters(string):
+    return string[(len(string) // 2) - 1:(len(string) // 2) + 2]
+
+# Path: main.py
+def get_middle_four_letters(string):
+    return string[(len(string) // 2) - 2:(len(string) // 2) + 2]
+
+# Path: main.py
+def get_middle_five_letters(string):
+    return string[(len(string) // 2) - 2:(len(string) // 2) + 3]
+
+# Path: main.py
+def get_middle_six_letters(string):
+    return string[(len(string) // 2) - 3:(len(string) // 2) + 3]
+
+# Path: main.py
+def get_middle_seven_letters(string):
+    return string[(len(string) // 2) - 3:(len(string) // 2) + 4]
+
+# Path: main.py
+def get_middle_eight_letters(string):
+    return string[(len(string) // 2) - 4:(len(string) // 2) + 4]
+
+# Path: main.py
+def get_middle_nine_letters(string):
+    return string[(len(string) // 2) - 4:(len(string) // 2) + 5]
+
+# Path: main.py
+def get_middle_ten_letters(string):
+    return string[(len(string) // 2) - 5:(len(string) // 2) + 5]
+
+# Path: main.py
+def get_middle_eleven_letters(string):
+    return string[(len(string) // 2) - 5:(len(string) // 2)
